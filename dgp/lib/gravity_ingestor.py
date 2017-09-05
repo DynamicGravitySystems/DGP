ad2a388e00891dc0650f27562e97f7886647ff4b
chris <bertinato@ldeo.columbia.edu>
2017-09-05 15:43:13 -0400

    Modified read_at1a to extract status bits.
    Status bits are extracted from the status field and concatenated onto the DataFrame. The 'gps_time' bit is used to remove all records for which the bit is False. Modified the sample_gravity.csv to accommodate tests for this feature.

--- dgp/lib/gravity_ingestor.py
+++ dgp/lib/gravity_ingestor.py
@@ -25,16 +25,39 @@ def safe_float(data, none_val=np.nan):
         return none_val
 
 def _extract_bits(bitfield, columns=None, as_bool=False):
+    """
+    Function that extracts bitfield values from integers.
+
+    A pandas.Series or numpy.array of integers is converted to a
+    pandas.DataFrame of 1/0 or True/False values for as many bits
+    there are in the integer - least signficant bit first - or for as many
+    column names that are given.
+
+    Parameters
+    ----------
+    bitfields : numpy.array or pandas.Series
+        16, 32, or 64-bit integers
+    columns : list, optional
+        If a list is given, then the column names are given to the resulting
+        columns in the order listed.
+    as_bool : bool, optional
+        If True, then values in returned DataFrame are type numpy.bool_
+
+    Returns
+    -------
+    pandas.DataFrame
+
+    """
     def _unpack_bits(n):
-        # assumes 32-bit number
         x = np.array(struct.unpack('4B', struct.pack('>I', n)), dtype=np.uint8)
         return np.flip(np.unpackbits(x), axis=0)
 
     data = bitfield.apply(_unpack_bits)
     df = pd.DataFrame(np.column_stack(list(zip(*data))))
 
-    # TO DO: simplify?
+    # set column names
     if columns is not None:
+        # remove fields from the end if not named
         if len(columns) < len(df.columns):
             df.drop(df.columns[range(len(columns), len(df.columns))], axis=1, inplace=True)
             df.columns = columns
@@ -48,21 +71,41 @@ def _extract_bits(bitfield, columns=None, as_bool=False):
     else:
         return df
 
-def read_at1a(path):
+def read_at1a(path, fill_with_nans=True):
     """
-    read_at1a :: String -> DataFrame
-
     Read and parse gravity data file from DGS AT1A (Airborne) meter.
+
     CSV Columns:
         gravity, long, cross, beam, temp, status, pressure, Etemp, GPSweek, GPSweekseconds
-    :param path: Filesystem path to gravity data file
-    :return: Pandas DataFrame of gravity data indexed by datetime, with UNIX timestamp converted from GPS time
+
+    Parameters
+    ----------
+    path : str
+        Filesystem path to gravity data file
+
+    Returns
+    -------
+    pandas.DataFrame
+        Gravity data indexed by datetime.
     """
     fields = ['gravity', 'long', 'cross', 'beam', 'temp', 'status', 'pressure', 'Etemp', 'GPSweek', 'GPSweekseconds']
 
     data = []
     df = pd.read_csv(path, header=None, engine='c', na_filter=False)
     df.columns = fields
+
+    # expand status field
+    status_field_names = ['clamp', 'unclamp', 'gps_sync', 'feedback', 'r1',
+                          'r2', 'ad_lock', 'rcvd', 'mode_1', 'mode_2',
+                          'plat_com', 'sens_com', 'gps_time', 'ad_sat',
+                          'long_accel', 'cross_accel', 'on_line']
+
+    status = _extract_bits(df['status'], columns=status_field_names,
+                          as_bool=True)
+
+    # df = df.append(status)
+    df = pd.concat([df, status], axis=1)
+    df.drop('status', axis=1, inplace=True)
 
     # create datetime index
     dt_list = []
@@ -71,9 +114,13 @@ def read_at1a(path):
 
     df.index = pd.DatetimeIndex(dt_list)
 
-    # resample
-    # offset_str = '{:d}U'.format(int(0.1 * 1e6))
-    offset_str = '100000U'
-    df = df.resample(offset_str).mean()
+    if fill_with_nans:
+        # select rows where time is synced with the GPS NMEA
+        df = df.loc[df['gps_time']]
+
+        # fill gaps with NaNs
+        interval = '100000U'
+        index = pd.date_range(df.index[0], df.index[-1], freq=interval)
+        df = df.reindex(index)
 
     return df
--- tests/test_gravity_ingestor.py
+++ tests/test_gravity_ingestor.py
@@ -26,9 +26,24 @@ class TestGravityIngestor(unittest.TestCase):
         expect = pd.DataFrame(data=array, columns=columns).astype(np.bool_)
         self.assertTrue(unpacked.equals(expect))
 
-    def test_import_at1a(self):
+    def test_import_at1a_no_fill_nans(self):
+        df = gi.read_at1a(os.path.abspath('tests/sample_gravity.csv'), fill_with_nans=False)
+        self.assertEqual(df.shape, (9, 26))
+
+        fields = ['gravity', 'long', 'cross', 'beam', 'temp', 'status', 'pressure', 'Etemp', 'GPSweek', 'GPSweekseconds']
+        # Test and verify an arbitrary line of data against the same line in the pandas DataFrame
+        line5 = [10061.171360, -0.026226, -0.094891, -0.093803, 62.253987, 21061, 39.690004, 52.263138, 1959, 219697.800]
+        sample_line = dict(zip(fields, line5))
+
+        self.assertEqual(df.gravity[4], sample_line['gravity'])
+        self.assertEqual(df.long[4], sample_line['long'])
+        # self.assertTrue(df.iloc[[2]].isnull().all)
+        self.assertFalse(df.gps_time[8])
+        # print(df[['gravity', 'long', 'cross']])
+
+    def test_import_at1a_fill_nans(self):
         df = gi.read_at1a(os.path.abspath('tests/sample_gravity.csv'))
-        self.assertEqual(df.shape, (10, 10))
+        self.assertEqual(df.shape, (9, 26))
 
         fields = ['gravity', 'long', 'cross', 'beam', 'temp', 'status', 'pressure', 'Etemp', 'GPSweek', 'GPSweekseconds']
         # Test and verify an arbitrary line of data against the same line in the pandas DataFrame
@@ -38,4 +53,4 @@ class TestGravityIngestor(unittest.TestCase):
         self.assertEqual(df.gravity[5], sample_line['gravity'])
         self.assertEqual(df.long[5], sample_line['long'])
         self.assertTrue(df.iloc[[2]].isnull().all)
-        print(df[['gravity', 'long', 'cross']])
+        # print(df[['gravity', 'long', 'cross']])
