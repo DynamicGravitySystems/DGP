37dfdfdb3d09ee7a5d1d4a7585ae878fa1c36673
Zac Brady <bradyzp@gmail.com>
2017-09-13 19:13:39 -0600

    Pull Gui into master - Switch to GitFlow (#22)
    * Fixed Qt pathing and import resolution to allow GUI launch as module.
    
    Launch gui with python -m dgp
    
    * Added tables to requirements.txt
    
    * Rewrite logic in gui/loader.py to use more efficient QThreading method
    
    * Rename gui/main.pyw to gui/main.py

--- dgp/__main__.py
+++ dgp/__main__.py
@@ -1,15 +1,18 @@
 # coding: utf-8
 
+import os
 import sys
-import time
 
+sys.path.append(os.path.dirname(__file__))
+
+# from dgp import resources_rc
 from dgp import resources_rc
 from PyQt5.QtWidgets import QApplication, QSplashScreen
-
 from dgp.gui.splash import SplashScreen
 
 """Program Main Entry Point - Loads SplashScreen GUI"""
 if __name__ == "__main__":
+    print("CWD: {}".format(os.getcwd()))
     app = QApplication(sys.argv)
     form = SplashScreen()
     sys.exit(app.exec_())
