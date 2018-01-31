1f00c51a3044519fe2ea3d27f34aab6b44e15335
chris <chrisbertinato@mac.com>
2018-01-31 17:29:19 -0500

    ENH: Added functionality to synchronize signals by cross correlation

---
+++ dgp/lib/timesync.py
@@ -0,0 +1,213 @@
+# coding=utf-8
+
+import numpy as np
+from pandas import DataFrame
+import pandas as pd
+from pandas.tseries.frequencies import to_offset
+from pandas.tseries.offsets import DateOffset
+from scipy.interpolate import interp1d
+import warnings
+
+
+def interpolate_1d_vector(vector: np.array, factor: int):
+    """
+    Interpolate i.e. up sample a give 1D vector by interpolation factor
+
+    Parameters
+    ----------
+    vector: np.array
+        1D Data Vector
+    factor: int
+        Interpolation factor
+
+    Returns
+    -------
+    np.array:
+        1D Array interpolated by 'factor'
+
+    """
+    x = np.arange(np.size(vector))
+    y = vector
+    f = interp1d(x, y)
+    # f = np.interp(x, x, y)
+
+    x_extended_by_factor = np.linspace(x[0], x[-1], np.size(x) * factor)
+    y_interpolated = np.zeros(np.size(x_extended_by_factor))
+
+    i = 0
+    for x in x_extended_by_factor:
+        y_interpolated[i] = f(x)
+        i += 1
+
+    return y_interpolated
+
+
+def find_time_delay(s1, s2, datarate=1, resolution: bool=False):
+    """
+    Finds the time shift or delay between two signals
+    If s1 is advanced to s2, then the delay is positive.
+
+    Parameters
+    ----------
+    s1: array-like
+    s2: array-like
+    datarate: int, optional
+        Input data sample rate in Hz. If objects with time-like indexes are
+        given in the first two arguments, then this argument is ignored.
+    resolution: bool
+        If False use data without oversampling
+        If True, calculates time delay with 10* oversampling
+
+    Returns
+    -------
+    Scalar:
+        Time shift between s1 and s2. If datarate is not specified, then the
+        delay is given in fractional samples. Otherwise, delay is given in
+        seconds. If both inputs have a time-like index, then the frequency
+        is inferred from there.
+
+    """
+
+    if hasattr(s1, 'index') and not hasattr(s2, 'index'):
+        warnings.warn('s2 has no index. Ignoring index for s1.', stacklevel=2)
+        in1 = s1.values
+        in2 = s2
+    elif not hasattr(s1, 'index') and hasattr(s2, 'index'):
+        warnings.warn('s1 has no index. Ignoring index for s1.', stacklevel=2)
+        in1 = s1
+        in2 = s2.values
+    elif hasattr(s1, 'index') and hasattr(s2, 'index'):
+        if not isinstance(s1.index, pd.DatetimeIndex):
+            warnings.warn('Index of s1 is not a DateTimeIndex. Ignoring both '
+                          'indexes.', stacklevel=2)
+            in1 = s1.values
+
+            try:
+                in2 = s2.values
+            except AttributeError:
+                in2 = s2
+
+        elif not isinstance(s2.index, pd.DatetimeIndex):
+            warnings.warn('Index of s2 is not a DateTimeIndex. Ignoring both '
+                          'indexes.', stacklevel=2)
+            in2 = s2.values
+
+            try:
+                in1 = s1.values
+            except AttributeError:
+                in1 = s1
+        else:
+            in1 = s1.values
+            in2 = s2.values
+
+            # TODO: Option to normalize the two indexes
+            if s1.index.freq is not None:
+                s1_freq = s1.index.freq
+            else:
+                s1_freq = s1.index.inferred_freq
+
+            if s2.index.freq is not None:
+                s2_freq = s2.index.freq
+            else:
+                s2_freq = s2.index.inferred_freq
+
+            if s1_freq != s2_freq:
+                raise ValueError('Indexes have different frequencies')
+
+            if s1_freq is None:
+                raise ValueError('Index frequency cannot be inferred')
+
+            freq = pd.to_timedelta(to_offset(s1_freq)).microseconds * 1e-6
+            datarate = 1 / freq
+
+    else:
+        in1 = s1
+        in2 = s2
+
+    lagwith = 200
+    len_s1 = len(in1)
+
+    if not resolution:
+        c = np.correlate(in1, in2, mode='full')
+        scale = datarate
+    else:
+        in1 = interpolate_1d_vector(in1, datarate)
+        in2 = interpolate_1d_vector(in2, datarate)
+        c = np.correlate(in1, in2, mode='full')
+        scale = datarate * 10
+
+    shift = np.linspace(-lagwith, lagwith, 2 * lagwith + 1)
+    corre = c[len_s1 - 1 - lagwith:len_s1 + lagwith]
+    maxi = np.argmax(corre)
+    dm1 = abs(corre[maxi] - corre[maxi - 1])
+    dp1 = abs(corre[maxi] - corre[maxi + 1])
+    if dm1 < dp1:
+        x = shift[maxi-2: maxi+1]
+        z = np.polyfit(x, corre[maxi - 2:maxi + 1], 2)
+    else:
+        z = np.polyfit(shift[maxi - 1:maxi + 2], corre[maxi - 1:maxi + 2], 2)
+
+    dt1 = z[1] / (2 * z[0])
+
+    return dt1 / scale
+
+
+def shift_frame(frame, delay):
+    return frame.tshift(delay * 1e6, freq='U')
+
+
+def shift_frames(gravity: DataFrame, gps: DataFrame, eotvos: DataFrame,
+                 datarate=10) -> DataFrame:
+    """
+    Synchronize and join a gravity and gps DataFrame (DF) into a single time
+    shifted DF.
+    Time lag/shift is found using the find_time_delay function, which cross
+    correlates the gravity channel with Eotvos corrections.
+    The DFs (gravity and gps) are then upsampled to a 1ms period using cubic
+    interpolation.
+    The Gravity DataFrame is then shifted by the time shift factor returned by
+    find_time_delay at ms precision.
+    We then join the GPS DF on the Gravity DF using a left join resulting in a
+    single DF with Gravity and GPS data at 1ms frequency.
+    Finally the joined DF is downsampled back to the original frequency 1/10Hz
+
+    Parameters
+    ----------
+    gravity: DataFrame
+        Gravity data DataFrame to time shift and join
+    gps: DataFrame
+        GPS/Trajectory DataFrame to correlate with Gravity data
+    eotvos: DataFrame
+        Eotvos correction for input Trajectory
+    datarate: int
+        Scalar datarate in Hz
+
+    Returns
+    -------
+    DataFrame:
+        Synchronized and joined DataFrame containing:
+            set{gravity.columns, gps.columns}
+        If gps contains duplicate column names relative to gravity DF, they will
+        be suffixed with '_gps'
+
+    """
+
+    # eotvos = calc_eotvos(gps['lat'].values, gps['longitude'].values,
+    #                      gps['ell_ht'].values, datarate)
+    delay = find_time_delay(gravity['gravity'].values, eotvos, 10)
+    time_shift = DateOffset(seconds=delay)
+
+    # Upsample and then shift:
+    grav_1ms = gravity.resample('1L').interpolate(method='cubic').fillna(method='pad')
+    gps_1ms = gps.resample('1L').interpolate(method='cubic').fillna(method='pad')
+    gravity_synced = grav_1ms.shift(freq=time_shift)  # type: DataFrame
+
+    # Join shifted DataFrames:
+    joined = gravity_synced.join(gps_1ms, how='left', rsuffix='_gps')
+
+    # Now downsample back to original period
+    down_sample = "{}S".format(1/datarate)
+    # TODO: What method to use when downsampling - mean, or some other method?
+    # Can use .apply() to apply custom filter/sampling method
+    return joined.resample(down_sample).mean()
+
--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
-from .context import dgp, APP
-
+import pytest
 import unittest
 import csv
 from pyqtgraph.flowchart import Flowchart
@@ -15,35 +14,44 @@ import dgp.lib.trajectory_ingestor as ti
 from dgp.lib.transform.gravity import (Eotvos, LatitudeCorrection,
                                        FreeAirCorrection)
 from dgp.lib.transform.filters import Detrend
-from dgp.lib.transform.operators import ScalarMultiply, ConcatenateSeries
+from dgp.lib.transform.operators import (ScalarMultiply, ConcatenateSeries,
+                                         AddSeries)
+from dgp.lib.transform.timeops import ComputeDelay, ShiftFrame
 
 
 class TestGraphNodes(unittest.TestCase):
-    def setUp(self):
-        # self.app = QtGui.QApplication([])
-        self.fc = Flowchart(terminals={
-            'data_in': {'io': 'in'},
-            'data_out': {'io': 'out'}
-        })
+    @classmethod
+    def setUpClass(cls):
+        cls.app = QtGui.QApplication([])
 
-        library = fclib.LIBRARY.copy()
-        library.addNodeType(Eotvos, [('Gravity',)])
-        library.addNodeType(LatitudeCorrection, [('Gravity',)])
-        library.addNodeType(FreeAirCorrection, [('Gravity',)])
-        library.addNodeType(Detrend, [('Filters',)])
-        library.addNodeType(ScalarMultiply, [('Operators',)])
-        library.addNodeType(ConcatenateSeries, [('Operators',)])
-        self.fc.setLibrary(library)
+        cls.library = fclib.LIBRARY.copy()
+        cls.library.addNodeType(Eotvos, [('Gravity',)])
+        cls.library.addNodeType(LatitudeCorrection, [('Gravity',)])
+        cls.library.addNodeType(FreeAirCorrection, [('Gravity',)])
+        cls.library.addNodeType(Detrend, [('Filters',)])
+        cls.library.addNodeType(ScalarMultiply, [('Operators',)])
+        cls.library.addNodeType(ConcatenateSeries, [('Operators',)])
 
         # Ensure gps_fields are ordered correctly relative to test file
         gps_fields = ['mdy', 'hms', 'lat', 'long', 'ortho_ht', 'ell_ht',
                       'num_stats', 'pdop']
-        self.data = ti.import_trajectory(
+        cls.data = ti.import_trajectory(
             'tests/sample_data/eotvos_short_input.txt',
             columns=gps_fields,
             skiprows=1,
             timeformat='hms'
         )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.app.exit()
+
+    def setUp(self):
+        self.fc = Flowchart(terminals={
+            'data_in': {'io': 'in'},
+            'data_out': {'io': 'out'}
+        })
+        self.fc.setLibrary(self.library)
 
     def test_eotvos_node(self):
         # TODO: More complete test that spans the range of possible inputs
@@ -190,18 +198,26 @@ class TestGraphNodes(unittest.TestCase):
 
 
 class TestBinaryOpsGraphNodes(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.app = QtGui.QApplication([])
+        cls.library = fclib.LIBRARY.copy()
+        cls.library.addNodeType(ConcatenateSeries, [('Operators',)])
+        cls.library.addNodeType(AddSeries, [('Operators',)])
+
     def setUp(self):
-        # self.app = QtGui.QApplication([])
-        # self.app = QtWidgets.QApplication([])
         self.fc = Flowchart(terminals={
             'A': {'io': 'in'},
             'B': {'io': 'in'},
             'data_out': {'io': 'out'}
         })
 
-        library = fclib.LIBRARY.copy()
-        library.addNodeType(ConcatenateSeries, [('Operators',)])
-        self.fc.setLibrary(library)
+        self.fc.setLibrary(self.library)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.app.exit()
 
     def test_concat_series(self):
         input_A = pd.Series(np.arange(0, 5), index=['A', 'B', 'C', 'D', 'E'])
@@ -221,3 +237,143 @@ class TestBinaryOpsGraphNodes(unittest.TestCase):
         # check that the indexes are equal
         self.assertTrue(input_A.index.identical(res.index))
         self.assertTrue(input_B.index.identical(res.index))
+
+    def test_add_series(self):
+        input_a = pd.Series(np.arange(0, 5), index=['A', 'B', 'C', 'D', 'E'])
+        input_b = pd.Series(np.arange(2, 7), index=['A', 'B', 'C', 'D', 'E'])
+        expected = input_a.astype(np.float64) + input_b.astype(np.float64)
+
+        fnode = self.fc.createNode('AddSeries', pos=(0, 0))
+        self.fc.connectTerminals(self.fc['A'], fnode['A'])
+        self.fc.connectTerminals(self.fc['B'], fnode['B'])
+        self.fc.connectTerminals(fnode['data_out'], self.fc['data_out'])
+
+        result = self.fc.process(A=input_a, B=input_b)
+        res = result['data_out']
+        self.assertTrue(res.equals(expected))
+
+
+class TestTimeOpsGraphNodes(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = QtGui.QApplication([])
+        cls.library = fclib.LIBRARY.copy()
+        cls.library.addNodeType(ComputeDelay, [('Time Ops',)])
+        cls.library.addNodeType(ShiftFrame, [('Time Ops',)])
+
+    def setUp(self):
+        self.fc = Flowchart(terminals={
+            's1': {'io': 'in'},
+            's2': {'io': 'in'},
+            'data_out': {'io': 'out'}
+        })
+
+        self.fc.setLibrary(self.library)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.app.exit()
+
+    def test_compute_delay_array(self):
+        rnd_offset = 1.1
+        t1 = np.linspace(1, 5000, 50000, dtype=np.float64)
+        t2 = t1 + rnd_offset
+        s1 = np.sin(0.8 * t1) + np.sin(0.2 * t1)
+        s2 = np.sin(0.8 * t2) + np.sin(0.2 * t2)
+
+        fnode = self.fc.createNode('ComputeDelay', pos=(0, 0))
+        self.fc.connectTerminals(self.fc['s1'], fnode['s1'])
+        self.fc.connectTerminals(self.fc['s2'], fnode['s2'])
+        self.fc.connectTerminals(fnode['data_out'], self.fc['data_out'])
+
+        result = self.fc.process(s1=s1, s2=s2)
+        res = result['data_out']
+
+        # TODO: Kludge to make the test pass. Consider whether to admit arrays in graph processing
+        self.assertAlmostEqual(rnd_offset, -res/10, places=2)
+
+    def test_compute_delay_timelike_index(self):
+        rnd_offset = 1.1
+        t1 = np.arange(0, 5000, 0.1, dtype=np.float64)
+        t2 = t1 + rnd_offset
+        s1 = np.sin(0.8 * t1) + np.sin(0.2 * t1)
+        s2 = np.sin(0.8 * t2) + np.sin(0.2 * t2)
+        now = pd.Timestamp.now()
+        index1 = now + pd.to_timedelta(t1, unit='s')
+        frame1 = pd.Series(s1, index=index1)
+        index2 = now + pd.to_timedelta(t2, unit='s')
+        frame2 = pd.Series(s2, index=index2)
+
+        fnode = self.fc.createNode('ComputeDelay', pos=(0, 0))
+        self.fc.connectTerminals(self.fc['s1'], fnode['s1'])
+        self.fc.connectTerminals(self.fc['s2'], fnode['s2'])
+        self.fc.connectTerminals(fnode['data_out'], self.fc['data_out'])
+
+        result = self.fc.process(s1=frame1, s2=frame2)
+        res = result['data_out']
+
+        self.assertAlmostEqual(rnd_offset, -res, places=2)
+
+    def test_compute_delay_timelike_index_raises(self):
+        rnd_offset = 1.1
+        t1 = np.arange(0, 5000, 0.1, dtype=np.float64)
+        t2 = t1 + rnd_offset
+        s1 = np.sin(0.8 * t1) + np.sin(0.2 * t1)
+        s2 = np.sin(0.8 * t2) + np.sin(0.2 * t2)
+        now = pd.Timestamp.now()
+        index1 = now + pd.to_timedelta(t1, unit='s')
+        frame1 = pd.Series(s1, index=index1)
+        frame2 = s2
+
+        fnode = self.fc.createNode('ComputeDelay', pos=(0, 0))
+        self.fc.connectTerminals(self.fc['s1'], fnode['s1'])
+        self.fc.connectTerminals(self.fc['s2'], fnode['s2'])
+        self.fc.connectTerminals(fnode['data_out'], self.fc['data_out'])
+
+        msg_expected = 's2 has no index. Ignoring index for s1.'
+        with self.assertWarns(UserWarning, msg=msg_expected):
+            self.fc.process(s1=frame1, s2=frame2)
+
+        frame1 = s1
+        index2 = now + pd.to_timedelta(t2, unit='s')
+        frame2 = pd.Series(s2, index=index2)
+
+        msg_expected = 's1 has no index. Ignoring index for s2.'
+        with self.assertWarns(UserWarning, msg=msg_expected):
+            self.fc.process(s1=frame1, s2=frame2)
+
+        frame1 = pd.Series(s1, index=index1)
+        index2 = now + pd.to_timedelta(t2, unit='s')
+        frame2 = pd.Series(s2)
+
+        msg_expected = ('Index of s2 is not a DateTimeIndex. Ignoring both '
+                        'indexes.')
+        with self.assertWarns(UserWarning, msg=msg_expected):
+            self.fc.process(s1=frame1, s2=frame2)
+
+        frame1 = pd.Series(s1)
+        index2 = now + pd.to_timedelta(t2, unit='s')
+        frame2 = pd.Series(s2, index=index2)
+
+        msg_expected = ('Index of s1 is not a DateTimeIndex. Ignoring both '
+                        'indexes.')
+        with self.assertWarns(UserWarning, msg=msg_expected):
+            self.fc.process(s1=frame1, s2=frame2)
+
+    def test_shift_frame(self):
+        test_input = pd.Series(np.arange(10))
+        index = pd.Timestamp.now() + pd.to_timedelta(np.arange(10), unit='s')
+        test_input.index = index
+        shifted_index = index.shift(110, freq='L')
+        expected = test_input.copy()
+        expected.index = shifted_index
+
+        fnode = self.fc.createNode('ShiftFrame', pos=(0, 0))
+        self.fc.connectTerminals(self.fc['s1'], fnode['frame'])
+        self.fc.connectTerminals(self.fc['s2'], fnode['delay'])
+        self.fc.connectTerminals(fnode['data_out'], self.fc['data_out'])
+
+        result = self.fc.process(s1=test_input, s2=0.11)
+        res = result['data_out']
+
+        self.assertTrue(res.equals(expected))
