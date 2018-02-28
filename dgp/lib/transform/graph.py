575afd72915c5451d7cf068cf93408d537bc6ce3
chris <chrisbertinato@mac.com>
2018-02-28 13:46:14 -0500

    Added TransformGraph class

--- dgp/lib/transform/graph.py
+++ dgp/lib/transform/graph.py
@@ -1,17 +1,84 @@
 # coding=utf-8
+from copy import copy
+from functools import partial
+
+
+class TransformGraph:
+    # TODO: Use magic methods for math ops where available
+
+    def __init__(self, graph):
+        self._transform_graph = graph
+        self._graph = self._make_graph()
+        self._order = self._graph._toposort()
+        self._results = None
+        self._graph_changed = False
+
+    @property
+    def graph(self):
+        return self._transform_graph
+
+    @graph.setter
+    def graph(self, g):
+        self._transform_graph = g
+        self._graph = self._make_graph()
+        self._order = self._graph._toposort()
+        self._graph_changed = True
+
+    @property
+    def results(self):
+        return self._results
+
+    def _make_graph(self):
+        adjacency_list = {k: [] for k in self._transform_graph}
+
+        for k in self._transform_graph:
+            node = self._transform_graph[k]
+            if isinstance(node, tuple):
+                args = list(node[1:])
+                for i in args:
+                    adjacency_list[k] += list(i)
+        return Graph(adjacency_list)
+
+    def execute(self):
+        if not self._graph_changed:
+            return self._results
+        else:
+            order = copy(self._order)
+            results = {}
+
+            def _tuple_to_func(tup):
+                func = tup[0]
+                args = []
+                for arg in tup[1:]:
+                    # TODO: Account for any kind of iterable, including generators.
+                    if isinstance(arg, list):
+                        args.append([results[x] for x in arg])
+                    else:
+                        args.append(results[arg])
+                new_tup = tuple([func] + args)
+                print(new_tup)
+                return partial(*new_tup)
+
+            while order:
+                k = order.pop()
+                node = self._transform_graph[k]
+                if isinstance(node, tuple):
+                    f = _tuple_to_func(node)
+                    results[k] = f()
+                else:
+                    results[k] = self._transform_graph[k]
+            self._results = results
+            self._graph_changed = False
+            return self._results
+
+    def __str__(self):
+        return str(self._transform_graph)
 
 
 class Graph:
-    def __init__(self):
-        self._graph = {}
-        self._toposort = []
-
-    @classmethod
-    def from_list(cls, inlist):
-        """ Generates a graph from the given adjacency list """
-        g = cls()
-        g._graph = inlist
-        return g
+    def __init__(self, graph):
+        self._graph = graph
+        self._topo = []
 
     def add_edge(self, u, v):
         """ Add an edge to the graph """
@@ -36,11 +103,21 @@ class Graph:
         stack.insert(0, node)
 
     def _toposort(self):
-        """ Topological sorting of the graph """
+        """
+        Topological sorting of the graph
+
+        Returns
+        -------
+            list
+                Order of execution as a stack
+        """
         visited = []
         stack = []
 
         for node in self._graph:
             self._visit(node, visited, stack)
         return stack
+
+    def __str__(self):
+        return str(self._graph)
 
