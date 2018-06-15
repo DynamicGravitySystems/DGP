0cabbde70e1286d8ecf624c0302b74f129dd21e5
Chris Bertinato <cbertinato@simonsfoundation.org>
2018-06-15 08:57:54 -0400

    Minor change in taylor_fir for pylint warning

--- dgp/lib/transform/derivatives.py
+++ dgp/lib/transform/derivatives.py
@@ -26,8 +26,6 @@ def central_difference(data_in, n=1, order=2, dt=0.1):
 def taylor_fir(data_in, n=1, dt=0.1):
     """ 10th order Taylor series FIR differentiator """
     coeff = np.array([1 / 1260, -5 / 504, 5 / 84, -5 / 21, 5 / 6, 0, -5 / 6, 5 / 21, -5 / 84, 5 / 504, -1 / 1260])
-    x = data_in
     for _ in range(1, n + 1):
-        y = convolve(x, coeff, mode='same')
-        x = y
+        y = convolve(data_in, coeff, mode='same')
     return y * (1/dt)**n
